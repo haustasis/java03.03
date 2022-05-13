cca802ff35db35ed1f6ba0d673b0598a38c1b9f5
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-05-13 11:23:58 +0200

    Floristeria constructors getters setters

--- sprint3/src/n1exercici1/Floristeria.java
+++ sprint3/src/n1exercici1/Floristeria.java
@@ -1,4 +1,71 @@
 package n1exercici1;
 
+import java.util.ArrayList;
+
 public class Floristeria {
+
+    // atributs
+    private String id;
+    private String nom;
+    private ArrayList<Arbre> llistaArbres;
+    private ArrayList<Flor> llistaFlors;
+    private ArrayList<Decoracio> llistaDecoracions;
+
+    // constructor sense parametres
+    public Floristeria() {
+        id = "";
+        nom = "";
+        llistaArbres = new ArrayList<>();
+        llistaFlors = new ArrayList<>();
+        llistaDecoracions = new ArrayList<>();
+    }
+
+    // constructor amb parametres
+    public Floristeria(String id, String nom, ArrayList<Arbre> llistaArbres, ArrayList<Flor> llistaFlors, ArrayList<Decoracio> llistaDecoracions) {
+        this.id = id;
+        this.nom = nom;
+        this.llistaArbres = llistaArbres;
+        this.llistaFlors = llistaFlors;
+        this.llistaDecoracions = llistaDecoracions;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public ArrayList<Arbre> getLlistaArbres() {
+        return llistaArbres;
+    }
+
+    public void setLlistaArbres(ArrayList<Arbre> llistaArbres) {
+        this.llistaArbres = llistaArbres;
+    }
+
+    public ArrayList<Flor> getLlistaFlors() {
+        return llistaFlors;
+    }
+
+    public void setLlistaFlors(ArrayList<Flor> llistaFlors) {
+        this.llistaFlors = llistaFlors;
+    }
+
+    public ArrayList<Decoracio> getLlistaDecoracions() {
+        return llistaDecoracions;
+    }
+
+    public void setLlistaDecoracions(ArrayList<Decoracio> llistaDecoracions) {
+        this.llistaDecoracions = llistaDecoracions;
+    }
 }
